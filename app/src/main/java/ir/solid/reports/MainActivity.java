d898deda3bfb52fff2654cbcca5f28a2ef762b62
Amir <amirh4liyan@gmail.com>
2023-08-06 12:51:12 +0330

    now MainActivity has functionality (need to create Activity for each Button)

--- app/src/main/java/ir/solid/reports/MainActivity.java
+++ app/src/main/java/ir/solid/reports/MainActivity.java
@@ -2,13 +2,38 @@ package ir.solid.reports;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button today_btn, add_report_btn, show_report_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        today_btn = findViewById(R.id.today_btn);
+        add_report_btn = findViewById(R.id.add_report_btn);
+        show_report_btn = findViewById(R.id.show_report_btn);
+
+        today_btn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, TodayActivity.class);
+            startActivity(intent);
+        });
+
+        add_report_btn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, AddActivity.class);
+            startActivity(intent);
+        });
+
+        show_report_btn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
